2149fc5466b558d2b08413f68084d9ddb1066f0b
Jason Wright <jasontwright@gmail.com>
2014-07-12 22:55:27 -0500

    No updates

--- test.py
+++ test.py
@@ -5,7 +5,9 @@
 # This request is keyed to only return 1 arrival
 
 import urllib
-u = urllib.urlopen('http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=26dd7d79ef8548648a163327672361ff&mapid=40080&max=1')
+apiKey = "26dd7d79ef8548648a163327672361ff"
+
+u = urllib.urlopen('http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key='+apiKey+'&mapid=40080&max=1')
 data = u.read()
 f = open('Sheridan.xml', 'wb')
 f.write(data)
